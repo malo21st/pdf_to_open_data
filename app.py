c867332c776dd6d32d3dd5b0a4d43e018dc3bcdd
malo21st <44739330+malo21st@users.noreply.github.com>
2023-04-19 21:44:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -96,8 +96,9 @@ if pdf_file != st.session_state['pdf_file'] and pdf_file:
             st.session_state['data_json'] = data_json
             st.session_state['data_df'] = data_df
             st.session_state['pdf_file'] = pdf_file
-    except Exception:
+    except Exception as e:
         st.error(f'エラーが発生しました。　再度お試し下さい。')
+        st.error(e)
 elif not st.session_state['data_df'].empty:
     st.dataframe(st.session_state['data_df'], height=423)
     st.plotly_chart(line_graph(st.session_state['data_df']))
